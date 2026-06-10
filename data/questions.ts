31a485abb19f58d71e04531f35eece711b7347bc
jackhlyons <jacklyonsdev@gmail.com>
2026-06-10 09:58:32 +0100

    Question data array created

---
+++ data/questions.ts
@@ -0,0 +1,32 @@
+export const questions = [
+  {
+    id: 1,
+    imageUrl: "/images/questions/photo1.jpg",
+    options: [1914, 1920, 1926],
+    correctAnswer: 1920,
+  },
+  {
+    id: 2,
+    imageUrl: "/images/questions/photo2.jpg",
+    options: [1999, 1994, 2003],
+    correctAnswer: 1994,
+  },
+  {
+    id: 3,
+    imageUrl: "/images/questions/photo3.jpg",
+    options: [1920, 1930, 1940],
+    correctAnswer: 1940,
+  },
+  {
+    id: 4,
+    imageUrl: "/images/questions/photo4.jpg",
+    options: [1964, 1970, 1951],
+    correctAnswer: 1970,
+  },
+  {
+    id: 5,
+    imageUrl: "/images/questions/photo5.jpg",
+    options: [1981, 1985, 1983],
+    correctAnswer: 1985,
+  },
+];
